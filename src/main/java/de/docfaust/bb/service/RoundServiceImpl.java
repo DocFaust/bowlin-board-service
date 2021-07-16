e1e4b1e701b9b3d9ccb9bed53e2b5f1e21624c22
Werner Faust <docfaust75@gmail.com>
2021-07-16 14:58:23 +0100

    optimize imports

--- src/main/java/de/docfaust/bb/service/RoundServiceImpl.java
+++ src/main/java/de/docfaust/bb/service/RoundServiceImpl.java
@@ -5,8 +5,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
